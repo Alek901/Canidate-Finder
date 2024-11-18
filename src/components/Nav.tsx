d8747ea7e3026b59717e6f638f0469d36e2c1f10
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-18 14:04:26 -0700

    adding the canidate finder to the Nav.tsx
    Adding one element to the Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,17 @@
+import { link } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
+    <nav className='bg-gray-800 p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <link to="/" className='text-white text-x1 font bold'>
+        Candidate Finder
+        </link>
+        
+      </div>
+    </nav>
     <div>Nav</div>
   )
 };
-
+// TODO: Add necessary code to display the navigation bar and link between the pages
 export default Nav;
