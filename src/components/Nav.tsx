4ae840e97a48d03a3fd67755b881650280af2201
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-18 14:15:49 -0700

    added the rest of the nav links
    Added the rest of the nav links that im planning to add and got rid of an error that was causing my page to not load

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,29 @@
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className='bg-gray-800 p-4'>
       <div className='container mx-auto flex justify-between items-center'>
-        <link to="/" className='text-white text-x1 font bold'>
+        <Link to="/" className='text-white text-x1 font bold'>
         Candidate Finder
-        </link>
+        </Link>
         <div className='flex space-x-6'>
           <link to="/" className='text-white hover:text-gray-300'>
           Home
-          </link>
-          
+          </Link>
+          <link to="/candidates" className='text-white hover:text-gray-300'>
+          Candidates
+          </Link>
+          <Link to="/jobs" className='text-white hover:text-gray-300'>
+          Jobs
+          </Link>
+          <Link to="/profile" className='text-white hover:text-gray-300'>
+          Profile
+          </Link>
         </div>
       </div>
     </nav>
-    <div>Nav</div>
-  )
+  );
 };
 // TODO: Add necessary code to display the navigation bar and link between the pages
 export default Nav;
