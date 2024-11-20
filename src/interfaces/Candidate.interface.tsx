2b52a65f940a3f8bd56cb407f07b6440163f18b4
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-19 20:42:59 -0700

    Added some comments
    Added some comments to better understand why I stoped and why I am turning it in how it is

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -8,3 +8,7 @@ export interface Candidate {
     html_url: string;
     company: string;
 }
+
+// I forgot how to pull from the API, so im just filling this in and what the todos say. :( 
+// I cant figure it out so im just turning stuff in to get some points and then come back and fill in the rest later.
+// Thanks for grading. :)
